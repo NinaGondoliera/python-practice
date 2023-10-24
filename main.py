7773c1331841803787e2c2f9e9a6c3302feffc11
NinaGondoliera <edaseppar@gmail.com>
2023-10-24 23:51:47 +0100

    Write greeting program

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
 
+# Ask the user for name, remove whitespace from str an capitalize user's name 
+name = input("What's your name? ").strip().title()
 
+#Greet user using their name
 
-print ("Hello world")
+print(f"Hello, {name}")
+
 
 ### Use docstrings to comment out stuff you dont want python to run!###
 
