51e20bb5c1359e8d2598c5f697aba6c492d32762
NinaGondoliera <edaseppar@gmail.com>
2023-10-24 22:04:02 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get('https://httpbin.org/ip')
+
+print('Your IP is {0}'.format(response.json()['origin']))
+
+
+
+print ("Hello world")
+
+### Use docstrings to comment out stuff you dont want python to run!###
